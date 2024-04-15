07c1124356a6fe83c784b514c7ade849e1a601a4
Sven Fehlmann <sven.fehlmann6@gmail.com>
2024-04-15 09:49:09 +0200

    Add User-Agent to all HTTP requests, to better imitate a real browser.

--- lib/src/utils/dio.dart
+++ lib/src/utils/dio.dart
@@ -1,10 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:dio_http2_adapter/dio_http2_adapter.dart';
 
-final dio = Dio()
-  ..httpClientAdapter = Http2Adapter(ConnectionManager(
-    idleTimeout: const Duration(seconds: 60),
-    // Ignore bad certificate
-    onClientCreate: (_, clientSetting) =>
-        clientSetting.onBadCertificate = (_) => true,
-  ));
+final dio = Dio(
+  BaseOptions(
+    headers: {
+      "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    },
+  ),
+)..httpClientAdapter = Http2Adapter(
+    ConnectionManager(
+      idleTimeout: const Duration(seconds: 60),
+      // Ignore bad certificate
+      onClientCreate: (_, clientSetting) =>
+          clientSetting.onBadCertificate = (_) => true,
+    ),
+  );
